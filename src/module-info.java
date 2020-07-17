09b2441223738e5cb243de74582b3374cfdf3b73
Xanxus555 <66057089+Xanxus555@users.noreply.github.com>
2020-07-17 20:38:07 +0800

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module BubbleHall {
+	exports com.tedu.element;
+	exports com.tedu.controller;
+	exports com.tedu.manager;
+	exports com.tedu.game;
+	exports com.tedu.show;
+	requires java.desktop;
+}
