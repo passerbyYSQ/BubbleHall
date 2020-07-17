22b29d9f517f95d7e658768953658c3b2dc6f1c8
Xanxus555 <66057089+Xanxus555@users.noreply.github.com>
2020-07-17 21:01:47 +0800

    删了

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module BubbleHall {
-	exports com.tedu.element;
-	exports com.tedu.controller;
-	exports com.tedu.manager;
-	exports com.tedu.game;
-	exports com.tedu.show;
-	requires java.desktop;
-}
